de19dc4d5f79a39b3cd58339b23a8312509abb97
Marks Bel <belehow.m.564717649@gmail.com>
2025-09-09 13:25:05 +0300

    shh_work_check

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['84.201.136.206', 'localhost', '127.0.0.1', 'kittygram.myftp.org']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
