35ed0e6383f8fc80eda397aaa22ff385c79b8720
Marks Bel <belehow.m.564717649@gmail.com>
2025-09-13 18:41:10 +0300

    remove_static_media

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -101,10 +101,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = '/backend_static/static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT  = '/app/media'  
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
