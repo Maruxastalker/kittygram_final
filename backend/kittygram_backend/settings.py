ee48af621b561cb4700d948ffb8519130d5bbc53
Marks Bel <belehow.m.564717649@gmail.com>
2025-09-14 10:52:46 +0300

    try_add_static_media_2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -99,12 +99,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 STATIC_URL = '/static/'
 STATIC_ROOT = '/backend_static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT  = '/app/media'  
+MEDIA_ROOT = '/app/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
